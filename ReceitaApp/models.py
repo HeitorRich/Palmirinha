7513b25362f4df0795831bfdaf288f771e5eb022
HEITOR RICHARD <heitorrichard48@gmail.com>
2024-04-02 22:13:25 -0300

    Adicionando atritubutos ao modelo receitas

--- ReceitaApp/models.py
+++ ReceitaApp/models.py
@@ -18,6 +18,9 @@ class Receita(models.Model):
     modo_de_preparo = models.TextField(max_length = 8000)
     grau_de_dificuldade = models.CharField(max_length=8, choices=dificuldade)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    tempo_de_preparo =  models.TimeField(max_length = 3, null = True, blank = True, help_text="Minutos")
+    creditos = models.CharField(max_length = 50, null = True, blank = True)
+    fonte = models.CharField(max_length = 100, null = True, blank = True)
 
 
     def __str__(self):
