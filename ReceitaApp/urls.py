982a9ca6ff771313c55ec53b0810d059995c5a5b
HEITOR RICHARD <heitorrichard48@gmail.com>
2024-04-04 22:16:08 -0300

    Adicionando as receitas do banco de dados nos html

--- ReceitaApp/urls.py
+++ ReceitaApp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
             #(caminho,   #view correspondente, name da url)
     path('', views.index, name='index'),
     path('receitas/', views.listar_receitas, name='receitas'),
+    path('receita/<int:id>', views.detalhes_receita, name='receita')
 ]
 
 
--- ReceitaApp/views.py
+++ ReceitaApp/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render
+from ReceitaApp.models import Receita, Categoria
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    categorias = Categoria.objects.all()
+
+    context = {
+        'categorias' : categorias
+    }
+    return render(request, 'index.html', context)
 
 def listar_receitas(request):
-    nome = 'Heitor'
-    ingredientes = ['Farinha', 'Ovos', 'Manteiga', 'Leite']
+    # buscar as receitas no banco de dados utilizando o ORM do django
+    receitas = Receita.objects.all()
+
+
 
     # dicionário que vai levar os dados para o template
     # chave : valor
     context = {
-        'Endereco' : 'Av. Marechal Tito',
-        'Bairro' : 'São Miguel Paulista',
-        'Cidade' : 'São Paulo',
-        'Estado' : 'SP',
-        'Nome' : nome,
-        'Ingredientes' : ingredientes
+        'receitas' : receitas
     }
 
     #qual template esse view vai retornar
     return render(request, 'receitas.html', context)
+
+def detalhes_receita(request, id):
+    #buscando a receita correspondente ao id informado
+    receita = Receita.objects.get(id = id)
+
+    context = {
+        'receita' : receita
+
+    }
+
+    return render(request, 'receita.html', context)
